f6b0bd550e6931cd299247d33b130a4ad83e709f
jake <jake.gads@gmail.com>
2019-08-30 13:53:26 -0400

    Yet another problem yeeted into the sun

--- ch02_students_jupyter_nb/Homework/DigitSeperation_Gadaleta.py
+++ ch02_students_jupyter_nb/Homework/DigitSeperation_Gadaleta.py
@@ -4,8 +4,8 @@ Write a script that inputs a five-digit integer from the user.
 Separate the number into its individual digits. Print them separated by three spaces each. 
 For example, if the user types in the number 42339, the script should print
 """
-
+# will and I took like a half to shave off extra lines we thing this is as short as we can get
 user = input("Gimme a number\t")
 result = ''
-for i in range(0,len(user)): result += "{}\t".format(user[i])
+for i in range(0,len(user)): result += "{}\t".format(user[i]) 
 print("Result : ", result)
