33f6c39ac2c141a9e4e3d7627aa61c3bac4b730a
Jake Gadaleta <jake.gads@gmail.com>
2019-09-04 09:33:06 -0400

    Had to dummy down my homework

--- ch02_students_jupyter_nb/Homework/DigitSeperation_Gadaleta.py
+++ ch02_students_jupyter_nb/Homework/DigitSeperation_Gadaleta.py
@@ -5,8 +5,22 @@ Separate the number into its individual digits. Print them separated by three sp
 For example, if the user types in the number 42339, the script should print
 """
 # will and I took like a half to shave off extra lines we thing this is as short as we can get
-user = input("Gimme a number\t") # Take the number but leave it as a string
-result = '' #stores the result
-for i in range(0,len(user)): 
-    result += "{}\t".format(user[i]) #pulls out each individual aspect 
-print("Result : ", result)
+user = int(input("Gimme a 5 Digit number\t")) # Take the number but leave it as a string
+
+one = user % 10 # gets the 1st character
+user //= 10 # removes that character
+
+two = user % 10
+user //= 10
+
+three = user % 10
+user //= 10
+
+four = user % 10
+user //= 10
+
+five = user % 10
+user //= 10
+
+print (five, '\t', four, '\t', three, '\t', two, '\t', one)
+# I really wish that I could have used lists 
