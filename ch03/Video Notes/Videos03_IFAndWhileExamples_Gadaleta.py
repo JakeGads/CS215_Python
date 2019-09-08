4dc05961cdcafc4220c4d508251d630e3837dd13
Jake <jake.gads@gmail.com>
2019-09-07 22:58:57 -0400

    Syncing current class notes

---
+++ ch03/Video Notes/Videos03_IFAndWhileExamples_Gadaleta.py
@@ -0,0 +1,86 @@
+grade = 85
+
+if grade >= 60:
+    print('Passed')
+
+if 1:
+    print('Nonzero values are True so this print')
+
+if 0:
+    print('0 values are flase so this isn\'t gonna print')
+    
+grade == 85
+
+grade == 58
+
+grade = 85
+
+if grade >= 60:
+    print('Passed')
+else:
+    print('Failed')
+
+grade = 57
+if grade >= 60:
+    print('Passed')
+else:
+    print('Failed')
+
+grade = 99
+
+if grade >= 60:
+    print('Passed')
+else:
+    print('Failed')
+
+grade = 85
+
+if grade >= 60:
+    print('Passed')
+else:
+    print('Failed')
+
+grade = 87
+
+if grade >= 60:
+    result = "passed"
+else:
+    result = "failed"
+
+result
+
+result = ('passed' if grade >= 60 else 'Failed')
+
+result
+
+grade = 49
+if grade >= 60:
+    print('Passed')
+else:
+    print('Failed')
+    print('You must take the course again')
+
+if grade >= 60:
+    print('Passed')
+else:
+    print('Failed')
+print('You must take the course again')
+
+grade = 77
+
+if grade >= 90:
+    print('A')
+elif grade >= 80:
+    print('B')
+elif grade >= 70:
+    print('C')
+elif grade >= 60:
+    print('D')
+else:
+    print('F')
+
+product = 3
+while product <= 50:
+    product *= 3
+
+print(product)
