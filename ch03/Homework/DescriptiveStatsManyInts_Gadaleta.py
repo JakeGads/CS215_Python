436c130888ba88509ebfd9fff24a73f77e62d47a
Jake Gadaleta <jake-gadaleta@pop-os.localdomain>
2019-09-09 10:27:53 -0400

    Syncing for computer switch

---
+++ ch03/Homework/DescriptiveStatsManyInts_Gadaleta.py
@@ -0,0 +1,12 @@
+"""
+3.8 (Arithmetic, Smallest and Largest) In Exercise 2.10, you wrote a script that input three integers, then displayed the sum, average, product, 
+smallest and largest of those values. 
+Reimplement your script with a loop that inputs four integers.
+
+Read #3.8. However, instead of asking the user to input 4 integers, let them input as many as they wish.  Use the letter "z" as a sentinel, meaning, when they enter the letter "z", we know they are done inputting integers.
+
+Name your file DescriptiveStatsManyInts_YourName.py.
+
+Of course, points will be taken off if the usual conventions are not followed.
+
+"""
