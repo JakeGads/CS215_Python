5f3b8311ed7d586b19440e5307dc354fb0139321
Jake Gadaleta <jake.gads@gmail.com>
2019-09-18 13:32:28 -0400

    Wrapped up the for loop lab

--- ch03/Lab/Shapes_Gadaleta.py
+++ ch03/Lab/Shapes_Gadaleta.py
@@ -1,24 +1,57 @@
+max_size = int(input('How large would you like the triangles:\t')) + 1
+max_size_s = max_size - 1
 #A
-for row in range(4):
+for row in range(max_size):
     for col in range(row):
         print('*', end='')
     print()
 print('\n\n')
 
-# B
-for row in range(4):
-    for col in range(4 - row):
+# B 
+for row in range(max_size):
+    for col in range(max_size - row -1):
         print('*', end='')
     print()
 print('\n\n')
 
-#C
-stars = 0
-for row in range(4,1,-1):
-    stars += 1
-    spaces = row - stars
-    for space in range(spaces):
+#C 
+for row in range(max_size):
+    for space in range(max_size_s - (max_size_s-row)):
         print(' ', end='')
-    for star in range(stars):
-        print('*',end='')
+    for star in range(max_size_s - row):
+        print('*', end='')
     print()
+print('\n\n')
+
+#D
+for row in range(max_size):
+    for space in range(max_size - row):
+        print(' ', end='')
+    for star in range(max_size - (max_size-row)):
+        print('*', end='')
+    print()
+
+
+'''
+    /\\     # 4, /, 0, \\
+   /  \\    # 3, /, 2, \\
+  /    \\   # 2, /, 4, \\
+ /      \\  # 1, / ,6, \\
+/________\\ # 0, /, 8, \\
+'''
+
+print('\n\n')
+
+space = 0
+for i in range(max_size):
+    for h in range(max_size - i):
+        print(' ', end='')
+    print('/', end='')
+    for h in range(space):
+        if i is not max_size - 1:
+            print('  ', end='')
+        else:
+            print('__', end='')
+    space += 1
+    print('\\')
+
