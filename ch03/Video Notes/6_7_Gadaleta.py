ec9cb54ca5346c1e75521f7fee7676269fc48dd6
Jake Gadaleta <jake.gads@gmail.com>
2019-09-19 21:57:11 -0400

    Video Notes

---
+++ ch03/Video Notes/6_7_Gadaleta.py
@@ -0,0 +1,27 @@
+total = 0
+grade_counter = 0
+grades = [98,76,71,87,83,90,57,79,82,94]
+
+for grade in grades:
+    total += grade
+    grade_counter += 1
+
+average = total / grade_counter
+print(f'Class average is {average}')
+
+
+total = 0
+grade_counter = 0
+
+grade = int(input('Enter grade, -1 to end: '))
+
+while grade != -1:
+    total += grade
+    grade_counter += 1
+    grade = int(input('Enter grade, -1 to end: '))
+
+if grade_counter != 0:
+    average = total / grade_counter
+    print(f'Class average is {round(average, 2)}')
+else:
+    print('No grades entered')
